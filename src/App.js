8f044d540ac0a6865ffb197333303e9e80af192f
samxaradzenika <murdoc746@gmail.com>
2023-04-16 23:41:09 +0400

    started up exercise project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import Button from './components/UI/Button/Button';
+
+function App() {
+   const [showParagraph, setShowParagraph] = useState(false);
+   const toggleParagraphHandler = () => {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+   };
+   return (
+      <div className="app">
+         <h1>Hi there!</h1>
+         {showParagraph && <p>this is new</p>}
+         <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      </div>
+   );
+}
+
+export default App;
